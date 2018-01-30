6fea62bacae51897f9666ec4c5b9e44563914a6f
Sargit <kaansargit@gmail.com>
2018-01-30 15:44:27 +0100

    Kleine begin

--- Haskell1.hs
+++ Haskell1.hs
@@ -1,5 +1,16 @@
 module Opdracht1
     where
 
+    type Lengte  = Double
+    type Breedte = Double
+    type Straal  = Double
 
-        
+    data Geofig = Vierkant | Driehoek | Circel | Rechthoek
+
+    data Colour = RGB Int Int Int
+
+    RGB :: Int -> Int -> Int -> Colour
+
+    -- Bro https://wiki.haskell.org/Colour
+    -- deze ook goeie https://stackoverflow.com/questions/18204308/haskell-type-vs-data-constructor
+
