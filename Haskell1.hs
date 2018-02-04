ef7c2e3780e3d1220549a227b49b1b6f43f7adb3
Sargit <kaansargit@gmail.com>
2018-02-04 04:19:08 +0100

    a

--- Haskell1.hs
+++ Haskell1.hs
@@ -4,12 +4,28 @@ module Opdracht1
         
     
 
-    data Geofig =   Vierkant Double Kleur
-                    | Driehoek Double Kleur
-                    | Circel Double Kleur
-                    | Rechthoek Double  Double Kleur
-                    deriving Show
+    data Geofig =   Vierkant Double Kleur String
+                    | Driehoek Double Kleur String
+                    | Circel Double Kleur String
+                    | Rechthoek Double  Double Kleur String
+                    deriving (Show,Eq)
 
 
-    data Kleur = Rood | Blauw | Geel | Zwart | Oranje deriving Show
+    data Kleur = Rood | Blauw | Geel | Oranje deriving (Show,Eq)
+
+    vierkant  = Vierkant  3.0 Blauw "vierkant"
+    driehoek  = Driehoek  5.0 Rood "driehoek"
+    circel    = Circel    1.5 Geel "circel" 
+    rechthoek = Rechthoek 4.65  5.21 Oranje "rechthoek"
+
+    oppervlakte :: Geofig->Double
+    oppervlakte x 
+                | (Geofig x  _ _ soort) == "vierkant" = zijde*zijde
+                    where zijde= (Geofig x  zijde _ _)
+                          soort= (Geofig x _ _ soort)
+
+
+
+
+    
 
